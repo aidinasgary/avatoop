5419a07c6e6c41c9e8019dd94b30da50e37dc7ee
mohammad malakooti <mrmohammadtafti@gmail.com>
2023-04-26 18:12:02 +0430

    editation!!

--- lib/ui/profile_screen.dart
+++ lib/ui/profile_screen.dart
@@ -13,9 +13,44 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'profil'
+        title: Text("آوا توپ!",
+          textDirection: TextDirection.rtl, style: TextStyle(fontFamily: 'Dubai'),
         ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 17, left: 225),
+            child: Text("حساب کاربری",
+              style: TextStyle(fontSize: 24, fontFamily: 'Dubai'),
+              textDirection: TextDirection.rtl,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 4),
+            width: 355,
+            height: 1.5,
+            color: Colors.black,
+          ),
+          Container(
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 20, left: 170),
+                  child: Image(image: AssetImage("assets/images/profile.png")),
+                ),
+                Column(
+                  children: [
+                    Container(),
+                    Container(),
+                    Container()
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
