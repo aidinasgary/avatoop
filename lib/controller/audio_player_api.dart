b83fe2370d97bcc340d8f152f24ccbac8e447aeb
aidinasgary <aidin351@gmail.com>
2023-07-17 13:31:15 +0330

    league build

---
+++ lib/controller/audio_player_api.dart
@@ -0,0 +1,49 @@
+import 'package:get/get.dart';
+import 'package:untitled16/constant/api_const.dart';
+import 'package:untitled16/controller/dioservice.dart';
+
+class AudioPlayerModel {
+  String? id;
+  String? podcastId;
+  String? title;
+  String? file;
+  String? lenght;
+
+  AudioPlayerModel();
+
+  AudioPlayerModel.fromJson(Map<String, dynamic> element) {
+    id = element["id"];
+    podcastId = element["podcast_id"];
+    file = element["file"];
+    title = element["title"];
+    lenght = element["lenght"];
+  }
+}
+
+class SingleAudioControllr extends GetxController {
+  var id;
+  SingleAudioControllr({this.id});
+
+  RxBool loading = false.obs;
+  Rx<AudioPlayerModel> audioPlayerModel = AudioPlayerModel().obs;
+  RxList<AudioPlayerModel> audioPlayerList = RxList();
+
+  @override
+  onInit() {
+    super.onInit();
+    getAudioFile();
+  }
+
+  getAudioFile() async {
+    loading.value = true;
+
+    var response =
+        await DioService().getMethod(ApiUrlConstant.podcastFiles + id);
+    if (response.stateCode == 200) {
+      for (var element in response.data['files']) {
+        audioPlayerList.add(AudioPlayerModel.fromJson(element));
+        loading.value = false;
+      }
+    }
+  }
+}
---
+++ lib/controller/dioservice.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+import 'package:dio/dio.dart';
+
+class DioService {
+  Future<dynamic> getMethod(String url) async {
+    Dio dio = Dio();
+    dio.options.headers['content-type'] = 'application/json';
+    return await dio
+        .get(url,
+            options: Options(responseType: ResponseType.json, method: 'GET'))
+        .then((response) {
+      log(response.toString() as num);
+      return response;
+    });
+  }
+}
